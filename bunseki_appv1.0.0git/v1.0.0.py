014c76c582ba54d2048fd9a72c01a2be8cd56d65
mamo921 <ayu.2008730@gmail.com>
2025-07-18 01:13:33 +0900

    Update v1.0.0.py

--- bunseki_appv1.0.0git/v1.0.0.py
+++ bunseki_appv1.0.0git/v1.0.0.py
@@ -11,23 +11,12 @@ import bcrypt
 import matplotlib.font_manager as fm # フォントマネージャーをインポート
 import os
 
-font_path = os.path.join(os.path.dirname(__file__), "static", "NotoSansJP-VariableFont_wght.otf")
-
-if os.path.exists(font_path):
-    font_prop = fm.FontProperties(fname=font_path)
-    plt.rcParams['font.family'] = font_prop.get_name()
-    japanese_font_available = True
-else:
-    font_prop = None
-    japanese_font_available = False
-    st.warning("日本語フォントが見つかりませんでした。グラフのラベルが文字化けする可能性があります。")
-
 # matplotlibのフォント設定
 plt.rcParams['axes.unicode_minus'] = False
 plt.rcParams['font.size'] = 10
 
 # フォントファイルのパス
-font_path = "bunseki_appv1.0.0git/static/NotoSansJP-VariableFont_wght.ttf"
+font_path = "static/NotoSansJP-VariableFont_wght.ttf"
 
 # フォント読み込みチェック
 if os.path.exists(font_path):
