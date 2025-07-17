0a5e6e79dcfb702de7c262ae4e3ac110dc563c6a
mamo921 <ayu.2008730@gmail.com>
2025-07-18 00:45:09 +0900

    Update v1.0.0.py

--- bunseki_appv1.0.0git/v1.0.0.py
+++ bunseki_appv1.0.0git/v1.0.0.py
@@ -483,7 +483,7 @@ def show_main_app():
 
                 # タイトル・軸ラベル
                 ax.set_title(get_graph_text(f"{selected_col}の値カウント", f"Value Counts of {selected_col}"), fontproperties=font_prop, **title_font)
-                ax.set_xlabel("テスト", fontproperties=font_prop)
+                ax.set_xlabel(get_graph_text(selected_col, selected_col), fontproperties=font_prop)
                 ax.set_ylabel(get_graph_text("カウント", "Count"), fontproperties=font_prop)
 
                 # ✅ x軸のTickラベルを完全に再描画する
